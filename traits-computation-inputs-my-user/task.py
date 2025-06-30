acdb742994f085b22327af18aa34edefc03c9ebd
EquiUPsrl <info@equiup.it>
2025-06-30 19:05:18 +0200

    traits-computation-inputs-my-user

---
+++ traits-computation-inputs-my-user/task.py
@@ -0,0 +1,25 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--outputs', action='store', type=str, required=True, dest='outputs')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+outputs = args.outputs.replace('"','')
+
+
+
+outputs["input_datain_file"] = 'Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
+outputs["input_operator_file"] = '2_FILEinformativo_OPERATORE.csv'
+
