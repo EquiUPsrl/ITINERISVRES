6082612fb66d0cd1438eb37d5bec96ab49b73270
EquiUPsrl <info@equiup.it>
2025-07-03 12:15:47 +0200

    untitled-my-user

--- untitled-my-user/task.py
+++ untitled-my-user/task.py
@@ -8,22 +8,17 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--param_file', action='store', type=str, required=True, dest='param_file')
+arg_parser.add_argument('--test', action='store', type=str, required=True, dest='test')
+
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
+test = args.test.replace('"','')
 
-param_file = args.param_file.replace('"','')
 
-conf_dati_file = conf_dati_file = 'work/input/TrainingData.csv'
 
-print(conf_dati_file)
-print(param_file)
-test = "hello"
+print(test)
 
-file_test = open("/tmp/test_" + id + ".json", "w")
-file_test.write(json.dumps(test))
-file_test.close()
