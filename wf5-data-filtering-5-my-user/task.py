5a48b6203c47022129409e44e8154092e68b7686
EquiUPsrl <info@equiup.it>
2025-07-25 12:31:15 +0200

    wf5-data-filtering-5-my-user

---
+++ wf5-data-filtering-5-my-user/task.py
@@ -0,0 +1,156 @@
+import os
+import requests
+import pandas as pd
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_datain', action='store', type=str, required=True, dest='param_datain')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+param_datain = args.param_datain.replace('"','')
+
+
+output_dir = '/tmp/data/output'
+
+print("Creo la cartella /tmp/data")
+os.makedirs("/tmp/data", exist_ok=True)
+
+print("Creo la cartella " + output_dir)
+os.makedirs(output_dir, exist_ok=True)
+
+input_filename = 'Overalldataset-harmonized2.csv'
+
+filepath = os.path.join("/tmp/data", input_filename)
+
+response = requests.get(param_datain)
+    
+if response.status_code == 200:
+    with open(filepath, "wb") as f:
+        f.write(response.content)
+    print("File CSV salvato con successo.")
+else:
+    print(f"Errore nel download: {response.status_code}")
+
+
+
+cluster = ['country','locality','year','month','parentEventID','eventID']
+taxlev = 'acceptedNameUsage'
+param = ['Density', 'totalbiovolume']
+threshold = 0.75
+
+filepath = os.path.join("/tmp/data", input_filename)
+
+dataset = pd.read_csv(filepath, sep=',', decimal='.', low_memory=False)
+
+if 'Density' not in dataset.columns:
+    dataset['Density'] = 1
+if 'Biovolume' not in dataset.columns:
+    dataset['Biovolume'] = 1
+if 'CellCarbonContent' not in dataset.columns:
+    dataset['CellCarbonContent'] = 1
+
+if 'totalbiovolume' not in dataset.columns:
+    dataset['totalbiovolume'] = dataset['Biovolume'] * dataset['Density']
+if 'totalcarboncontent' not in dataset.columns:
+    dataset['totalcarboncontent'] = dataset['CellCarbonContent'] * dataset['Density']
+
+if cluster[0] != "WHOLE":
+    if len(cluster) > 1:
+        ID = dataset[cluster].apply(lambda x: '.'.join(x.astype(str)), axis=1)
+    else:
+        ID = dataset[cluster[0]]
+else:
+    ID = pd.Series(['all'] * len(dataset))
+
+if 'Density' in param:
+    IDZ = ID.unique()
+    IDLIST = {}
+    for idz in IDZ:
+        ddd = dataset[ID == idz]
+        totz = ddd['Density'].sum()
+        matz = ddd.groupby(taxlev)['Density'].sum() / totz
+        matz = matz.sort_values(ascending=False)
+        k = 1
+        trs = matz.max()
+        while trs < threshold:
+            matz.iloc[k] = matz.iloc[k] + matz.iloc[k-1]
+            trs = matz.iloc[k]
+            k += 1
+        matzx = matz.iloc[:k]
+        IDLIST[idz] = ddd[ddd[taxlev].isin(matzx.index)]
+
+    dataset_d = pd.concat(IDLIST.values())
+
+else:
+    dataset_d = pd.DataFrame()
+
+if 'totalbiovolume' in param:
+    IDZ = ID.unique()
+    IDLIST = {}
+    for idz in IDZ:
+        ddd = dataset[ID == idz]
+        totz = ddd['totalbiovolume'].sum()
+        matz = ddd.groupby(taxlev)['totalbiovolume'].sum() / totz
+        matz = matz.sort_values(ascending=False)
+        k = 1
+        trs = matz.max()
+        while trs < threshold:
+            matz.iloc[k] = matz.iloc[k] + matz.iloc[k-1]
+            trs = matz.iloc[k]
+            k += 1
+        matzx = matz.iloc[:k]
+        IDLIST[idz] = ddd[ddd[taxlev].isin(matzx.index)]
+
+    dataset_b = pd.concat(IDLIST.values())
+
+else:
+    dataset_b = pd.DataFrame()
+
+if 'totalcarboncontent' in param:
+    IDZ = ID.unique()
+    IDLIST = {}
+    for idz in IDZ:
+        ddd = dataset[ID == idz]
+        totz = ddd['totalcarboncontent'].sum()
+        matz = ddd.groupby(taxlev)['totalcarboncontent'].sum() / totz
+        matz = matz.sort_values(ascending=False)
+        k = 1
+        trs = matz.max()
+        while trs < threshold:
+            matz.iloc[k] = matz.iloc[k] + matz.iloc[k-1]
+            trs = matz.iloc[k]
+            k += 1
+        matzx = matz.iloc[:k]
+        IDLIST[idz] = ddd[ddd[taxlev].isin(matzx.index)]
+
+    dataset_c = pd.concat(IDLIST.values())
+
+else:
+    dataset_c = pd.DataFrame()
+
+dtc = pd.concat([dataset_d, dataset_b, dataset_c])
+
+final = dtc.drop_duplicates()
+
+threshold_str = f'ThresholdValue_{threshold}'
+
+output_file = f'{output_dir}/FilteringOutput_{threshold_str}.csv'
+final.to_csv(output_file, index=False, sep=';', decimal='.', quoting=0)
+
+print(f"Output saved to {output_file}")
+
+file_output_file = open("/tmp/output_file_" + id + ".json", "w")
+file_output_file.write(json.dumps(output_file))
+file_output_file.close()
