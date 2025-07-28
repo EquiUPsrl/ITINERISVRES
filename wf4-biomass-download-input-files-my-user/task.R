9926f554ec80d471a4bcb20ba0bcf5425fa2f7b5
EquiUPsrl <info@equiup.it>
2025-07-28 16:54:04 +0200

    wf4-biomass-download-input-files-my-user

---
+++ wf4-biomass-download-input-files-my-user/task.R
@@ -0,0 +1,174 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("caret", quietly = TRUE)) {
+	install.packages("caret", repos="http://cran.us.r-project.org")
+}
+library(caret)
+if (!requireNamespace("doFuture", quietly = TRUE)) {
+	install.packages("doFuture", repos="http://cran.us.r-project.org")
+}
+library(doFuture)
+if (!requireNamespace("doParallel", quietly = TRUE)) {
+	install.packages("doParallel", repos="http://cran.us.r-project.org")
+}
+library(doParallel)
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+	install.packages("dplyr", repos="http://cran.us.r-project.org")
+}
+library(dplyr)
+if (!requireNamespace("e1071", quietly = TRUE)) {
+	install.packages("e1071", repos="http://cran.us.r-project.org")
+}
+library(e1071)
+if (!requireNamespace("foreach", quietly = TRUE)) {
+	install.packages("foreach", repos="http://cran.us.r-project.org")
+}
+library(foreach)
+if (!requireNamespace("future", quietly = TRUE)) {
+	install.packages("future", repos="http://cran.us.r-project.org")
+}
+library(future)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("iml", quietly = TRUE)) {
+	install.packages("iml", repos="http://cran.us.r-project.org")
+}
+library(iml)
+if (!requireNamespace("kernlab", quietly = TRUE)) {
+	install.packages("kernlab", repos="http://cran.us.r-project.org")
+}
+library(kernlab)
+if (!requireNamespace("MASS", quietly = TRUE)) {
+	install.packages("MASS", repos="http://cran.us.r-project.org")
+}
+library(MASS)
+if (!requireNamespace("Metrics", quietly = TRUE)) {
+	install.packages("Metrics", repos="http://cran.us.r-project.org")
+}
+library(Metrics)
+if (!requireNamespace("nnet", quietly = TRUE)) {
+	install.packages("nnet", repos="http://cran.us.r-project.org")
+}
+library(nnet)
+if (!requireNamespace("randomForest", quietly = TRUE)) {
+	install.packages("randomForest", repos="http://cran.us.r-project.org")
+}
+library(randomForest)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("xgboost", quietly = TRUE)) {
+	install.packages("xgboost", repos="http://cran.us.r-project.org")
+}
+library(xgboost)
+if (!requireNamespace("scales", quietly = TRUE)) {
+	install.packages("scales", repos="http://cran.us.r-project.org")
+}
+library(scales)
+if (!requireNamespace("tidyr", quietly = TRUE)) {
+	install.packages("tidyr", repos="http://cran.us.r-project.org")
+}
+library(tidyr)
+if (!requireNamespace("reshape2", quietly = TRUE)) {
+	install.packages("reshape2", repos="http://cran.us.r-project.org")
+}
+library(reshape2)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="task id")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+id <- gsub('"', '', opt$id)
+
+{'name': 'conf_base_path', 'assignation': "conf_base_path<-'/tmp/data/'"}
+{'name': 'conf_output_path', 'assignation': "conf_output_path<-'/tmp/data/output/'"}
+
+print("Running the cell")
+download_to_folder <- function(url, folder, filename = NULL, binary = TRUE) {
+  if (!dir.exists(folder)) {
+    dir.create(folder, recursive = TRUE)
+  }
+  
+  if (is.null(filename)) {
+    filename <- basename(url)
+  }
+  
+  dest_file <- file.path(folder, filename)
+  
+  mode <- if (binary) "wb" else "w"
+  
+  download.file(url, destfile = dest_file, mode = mode)
+
+  cat("File saved: ", dest_file)
+  
+  return(dest_file)
+}
+
+training_file <- download_to_folder(
+  url = param_training_file,
+  folder = conf_base_path
+)
+
+prediction_file <- download_to_folder(
+  url = param_prediction_file,
+  folder = conf_base_path
+)
+
+parameter_file <- download_to_folder(
+  url = param_parameter_file,
+  folder = conf_base_path
+)
+# capturing outputs
+print('Serialization of parameter_file')
+file <- file(paste0('/tmp/parameter_file_', id, '.json'))
+writeLines(toJSON(parameter_file, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of prediction_file')
+file <- file(paste0('/tmp/prediction_file_', id, '.json'))
+writeLines(toJSON(prediction_file, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of training_file')
+file <- file(paste0('/tmp/training_file_', id, '.json'))
+writeLines(toJSON(training_file, auto_unbox=TRUE), file)
+close(file)
