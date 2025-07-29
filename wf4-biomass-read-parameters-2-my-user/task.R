032e0e78af0c90b2bdcc4557fb5ef1d6cdd93011
EquiUPsrl <info@equiup.it>
2025-07-29 16:40:48 +0200

    wf4-biomass-read-parameters-2-my-user

---
+++ wf4-biomass-read-parameters-2-my-user/task.R
@@ -0,0 +1,167 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("caret", quietly = TRUE)) {
+	install.packages("caret", repos="http://cran.us.r-project.org")
+}
+library(caret)
+if (!requireNamespace("doFuture", quietly = TRUE)) {
+	install.packages("doFuture", repos="http://cran.us.r-project.org")
+}
+library(doFuture)
+if (!requireNamespace("doParallel", quietly = TRUE)) {
+	install.packages("doParallel", repos="http://cran.us.r-project.org")
+}
+library(doParallel)
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+	install.packages("dplyr", repos="http://cran.us.r-project.org")
+}
+library(dplyr)
+if (!requireNamespace("e1071", quietly = TRUE)) {
+	install.packages("e1071", repos="http://cran.us.r-project.org")
+}
+library(e1071)
+if (!requireNamespace("foreach", quietly = TRUE)) {
+	install.packages("foreach", repos="http://cran.us.r-project.org")
+}
+library(foreach)
+if (!requireNamespace("future", quietly = TRUE)) {
+	install.packages("future", repos="http://cran.us.r-project.org")
+}
+library(future)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("iml", quietly = TRUE)) {
+	install.packages("iml", repos="http://cran.us.r-project.org")
+}
+library(iml)
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+if (!requireNamespace("kernlab", quietly = TRUE)) {
+	install.packages("kernlab", repos="http://cran.us.r-project.org")
+}
+library(kernlab)
+if (!requireNamespace("MASS", quietly = TRUE)) {
+	install.packages("MASS", repos="http://cran.us.r-project.org")
+}
+library(MASS)
+if (!requireNamespace("Metrics", quietly = TRUE)) {
+	install.packages("Metrics", repos="http://cran.us.r-project.org")
+}
+library(Metrics)
+if (!requireNamespace("nnet", quietly = TRUE)) {
+	install.packages("nnet", repos="http://cran.us.r-project.org")
+}
+library(nnet)
+if (!requireNamespace("randomForest", quietly = TRUE)) {
+	install.packages("randomForest", repos="http://cran.us.r-project.org")
+}
+library(randomForest)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("xgboost", quietly = TRUE)) {
+	install.packages("xgboost", repos="http://cran.us.r-project.org")
+}
+library(xgboost)
+if (!requireNamespace("scales", quietly = TRUE)) {
+	install.packages("scales", repos="http://cran.us.r-project.org")
+}
+library(scales)
+if (!requireNamespace("tidyr", quietly = TRUE)) {
+	install.packages("tidyr", repos="http://cran.us.r-project.org")
+}
+library(tidyr)
+if (!requireNamespace("reshape2", quietly = TRUE)) {
+	install.packages("reshape2", repos="http://cran.us.r-project.org")
+}
+library(reshape2)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--params_path"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--id"), action="store", default=NA, type="character", help="task id")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving params_path")
+var = opt$params_path
+print(var)
+var_len = length(var)
+print(paste("Variable params_path has length", var_len))
+
+params_path <- gsub("\"", "", opt$params_path)
+id <- gsub('"', '', opt$id)
+
+
+print("Running the cell")
+library(jsonlite)
+
+training_url = ''
+prediction_url = ''
+parameter_url = ''
+
+if (file.exists(params_path)) {
+  params <- fromJSON(params_path)
+
+  training_url   = params$param_training_file
+  prediction_url = params$param_prediction_file
+  parameter_url  = params$param_parameter_file
+
+  cat("✅ Parametri caricati correttamente.\n")
+} else {
+  cat(paste("❌ File dei parametri non trovato:", params_path))
+}
+# capturing outputs
+print('Serialization of parameter_url')
+file <- file(paste0('/tmp/parameter_url_', id, '.json'))
+writeLines(toJSON(parameter_url, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of prediction_url')
+file <- file(paste0('/tmp/prediction_url_', id, '.json'))
+writeLines(toJSON(prediction_url, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of training_url')
+file <- file(paste0('/tmp/training_url_', id, '.json'))
+writeLines(toJSON(training_url, auto_unbox=TRUE), file)
+close(file)
