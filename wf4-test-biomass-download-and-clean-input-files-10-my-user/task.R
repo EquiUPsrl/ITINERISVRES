da6aedf3f64cab278047fdd3fa382b59b4752de7
EquiUPsrl <info@equiup.it>
2025-07-29 15:28:35 +0200

    wf4-test-biomass-download-and-clean-input-files-10-my-user

---
+++ wf4-test-biomass-download-and-clean-input-files-10-my-user/task.R
@@ -0,0 +1,78 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--params_path"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--id"), action="store", default=NA, type="character", help="task id")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving params_path")
+var = opt$params_path
+print(var)
+var_len = length(var)
+print(paste("Variable params_path has length", var_len))
+
+params_path <- gsub("\"", "", opt$params_path)
+id <- gsub('"', '', opt$id)
+
+
+print("Running the cell")
+library(jsonlite)
+
+param_training_file = ''
+param_prediction_file = ''
+param_parameter_file = ''
+
+if (file.exists(params_path)) {
+  params <- fromJSON(params_path)
+
+  param_training_file   = params$param_training_file
+  param_prediction_file = params$param_prediction_file
+  param_parameter_file  = params$param_parameter_file
+
+  cat("✅ Parametri caricati correttamente.\n")
+} else {
+  stop(paste("❌ File dei parametri non trovato:", params_path))
+}
