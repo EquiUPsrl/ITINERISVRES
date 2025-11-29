a0f07544e359c07c500200c79c3da94e8639781c
EquiUPsrl <info@equiup.it>
2025-11-29 17:09:16 +0100

    wf4-biomass-violin-plot-my-user

---
+++ wf4-biomass-violin-plot-my-user/task.R
@@ -0,0 +1,188 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("caret", quietly = TRUE)) {
+	install.packages("caret", repos="http://cran.us.r-project.org")
+}
+library(caret)
+if (!requireNamespace("doFuture", quietly = TRUE)) {
+	install.packages("doFuture", repos="http://cran.us.r-project.org")
+}
+library(doFuture)
+if (!requireNamespace("doParallel", quietly = TRUE)) {
+	install.packages("doParallel", repos="http://cran.us.r-project.org")
+}
+library(doParallel)
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+	install.packages("dplyr", repos="http://cran.us.r-project.org")
+}
+library(dplyr)
+if (!requireNamespace("e1071", quietly = TRUE)) {
+	install.packages("e1071", repos="http://cran.us.r-project.org")
+}
+library(e1071)
+if (!requireNamespace("fastICA", quietly = TRUE)) {
+	install.packages("fastICA", repos="http://cran.us.r-project.org")
+}
+library(fastICA)
+if (!requireNamespace("foreach", quietly = TRUE)) {
+	install.packages("foreach", repos="http://cran.us.r-project.org")
+}
+library(foreach)
+if (!requireNamespace("future", quietly = TRUE)) {
+	install.packages("future", repos="http://cran.us.r-project.org")
+}
+library(future)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("iml", quietly = TRUE)) {
+	install.packages("iml", repos="http://cran.us.r-project.org")
+}
+library(iml)
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+if (!requireNamespace("kernlab", quietly = TRUE)) {
+	install.packages("kernlab", repos="http://cran.us.r-project.org")
+}
+library(kernlab)
+if (!requireNamespace("MASS", quietly = TRUE)) {
+	install.packages("MASS", repos="http://cran.us.r-project.org")
+}
+library(MASS)
+if (!requireNamespace("Metrics", quietly = TRUE)) {
+	install.packages("Metrics", repos="http://cran.us.r-project.org")
+}
+library(Metrics)
+if (!requireNamespace("nnet", quietly = TRUE)) {
+	install.packages("nnet", repos="http://cran.us.r-project.org")
+}
+library(nnet)
+if (!requireNamespace("randomForest", quietly = TRUE)) {
+	install.packages("randomForest", repos="http://cran.us.r-project.org")
+}
+library(randomForest)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("xgboost", quietly = TRUE)) {
+	install.packages("xgboost", repos="http://cran.us.r-project.org")
+}
+library(xgboost)
+if (!requireNamespace("tidyr", quietly = TRUE)) {
+	install.packages("tidyr", repos="http://cran.us.r-project.org")
+}
+library(tidyr)
+if (!requireNamespace("scales", quietly = TRUE)) {
+	install.packages("scales", repos="http://cran.us.r-project.org")
+}
+library(scales)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--model_dir"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--target_variable"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--target_variable_uom"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--id"), action="store", default=NA, type="character", help="task id")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving model_dir")
+var = opt$model_dir
+print(var)
+var_len = length(var)
+print(paste("Variable model_dir has length", var_len))
+
+model_dir <- gsub("\"", "", opt$model_dir)
+print("Retrieving target_variable")
+var = opt$target_variable
+print(var)
+var_len = length(var)
+print(paste("Variable target_variable has length", var_len))
+
+target_variable <- gsub("\"", "", opt$target_variable)
+print("Retrieving target_variable_uom")
+var = opt$target_variable_uom
+print(var)
+var_len = length(var)
+print(paste("Variable target_variable_uom has length", var_len))
+
+target_variable_uom <- gsub("\"", "", opt$target_variable_uom)
+id <- gsub('"', '', opt$id)
+
+
+print("Running the cell")
+library(caret)
+library(ggplot2)
+library(tidyr)
+
+test_data  <- readRDS(file.path(model_dir, "test_data.rds"))
+best_model  <- readRDS(file.path(model_dir, "best_model.rds"))
+uom <- target_variable_uom
+
+predictions_test <- predict(best_model, newdata = test_data)
+
+results_test_df <- data.frame(
+  Observed = test_data[[target_variable]],
+  Predicted = predictions_test
+)
+
+results_test_long <- results_test_df %>%
+  pivot_longer(cols = c("Observed", "Predicted"),
+               names_to = "Type",
+               values_to = "Value")
+
+violin_plot <- ggplot(results_test_long, aes(x = Type, y = Value, fill = Type)) +
+  geom_violin(trim = FALSE) +  # Mostra tutta la distribuzione
+  scale_fill_manual(values = c("blue", "red")) +  # Imposta colori per dati reali e predetti
+  labs(title = paste("Observed vs Predicted Values ", target_variable),
+       x = " Data Type",
+       y = paste("Value of ", target_variable, "(", uom, ")")) +
+  theme_minimal() +
+  theme(legend.position = "none")  # Rimuovi la legenda
+
+print(violin_plot)
+
+plot_path_violin_test <- file.path(model_dir, "violin_plot_test_set.png")
+ggsave(filename = plot_path_violin_test, plot = violin_plot, width = 8, height = 6)
+
+cat("Violin plot saved in:", plot_path_violin_test, "\n")
