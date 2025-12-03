2552948327719744f55d9bb792b1061d6c4efd09
EquiUPsrl <info@equiup.it>
2025-12-03 18:10:39 +0100

    wf4-biomass-extreme-gradient-boosting-2-my-user

--- wf4-biomass-extreme-gradient-boosting-2-my-user/task.R
+++ wf4-biomass-extreme-gradient-boosting-2-my-user/task.R
@@ -74,14 +74,14 @@ if (!requireNamespace("xgboost", quietly = TRUE)) {
 	install.packages("xgboost", repos="http://cran.us.r-project.org")
 }
 library(xgboost)
-if (!requireNamespace("remotes", quietly = TRUE)) {
-	install.packages("remotes", repos="http://cran.us.r-project.org")
-}
-library(remotes)
 if (!requireNamespace("tidyr", quietly = TRUE)) {
 	install.packages("tidyr", repos="http://cran.us.r-project.org")
 }
 library(tidyr)
+if (!requireNamespace("scales", quietly = TRUE)) {
+	install.packages("scales", repos="http://cran.us.r-project.org")
+}
+library(scales)
 
 
 
@@ -607,3 +607,20 @@ cat("Table with input data and forecasts saved in: ", output_path, "\n")
 
 saveRDS(train_data, file = file.path(model_dir, "train_data.rds"))
 saveRDS(test_data,  file = file.path(model_dir, "test_data.rds"))
+# capturing outputs
+print('Serialization of model_dir')
+file <- file(paste0('/tmp/model_dir_', id, '.json'))
+writeLines(toJSON(model_dir, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of predictors')
+file <- file(paste0('/tmp/predictors_', id, '.json'))
+writeLines(toJSON(predictors, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of target_variable')
+file <- file(paste0('/tmp/target_variable_', id, '.json'))
+writeLines(toJSON(target_variable, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of target_variable_uom')
+file <- file(paste0('/tmp/target_variable_uom_', id, '.json'))
+writeLines(toJSON(target_variable_uom, auto_unbox=TRUE), file)
+close(file)
