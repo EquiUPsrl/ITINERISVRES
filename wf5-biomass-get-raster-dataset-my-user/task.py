ac335117a553822a41338690e12bbf0621647677
EquiUPsrl <info@equiup.it>
2025-12-11 18:42:18 +0100

    wf5-biomass-get-raster-dataset-my-user

---
+++ wf5-biomass-get-raster-dataset-my-user/task.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--raster_dataset_file', action='store', type=str, required=True, dest='raster_dataset_file')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+raster_dataset_file = args.raster_dataset_file.replace('"','')
+
+
+
+df = pd.read_csv(raster_dataset_file, sep=";")
+
+zip_files = []
+oceancolor = []
+ocean_productivity = []
+
+start_year = int(df.iloc[0]["start_year"])
+end_year = int(df.iloc[0]["end_year"])
+
+for _, row in df.iterrows():
+    product = row['products']
+    download_type = row['download']
+    zip_link = row['zip_file']
+
+    if pd.notna(zip_link) and zip_link.strip() != "":
+        zip_files.append(product + "||" + zip_link.strip())
+    elif download_type == "oceancolor" and (pd.isna(zip_link) or zip_link.strip() == ""):
+        oceancolor.append(product)
+    elif download_type == "ocean_productivity" and (pd.isna(zip_link) or zip_link.strip() == ""):
+        ocean_productivity.append(product)
+
+file_end_year = open("/tmp/end_year_" + id + ".json", "w")
+file_end_year.write(json.dumps(end_year))
+file_end_year.close()
+file_ocean_productivity = open("/tmp/ocean_productivity_" + id + ".json", "w")
+file_ocean_productivity.write(json.dumps(ocean_productivity))
+file_ocean_productivity.close()
+file_oceancolor = open("/tmp/oceancolor_" + id + ".json", "w")
+file_oceancolor.write(json.dumps(oceancolor))
+file_oceancolor.close()
+file_start_year = open("/tmp/start_year_" + id + ".json", "w")
+file_start_year.write(json.dumps(start_year))
+file_start_year.close()
+file_zip_files = open("/tmp/zip_files_" + id + ".json", "w")
+file_zip_files.write(json.dumps(zip_files))
+file_zip_files.close()
