456497ed4b7ead993d26003dda751ae14f790f16
EquiUPsrl <info@equiup.it>
2026-01-21 19:46:35 +0100

    wf4-biomass-extreme-gradient-boosting-my-user

--- wf4-biomass-extreme-gradient-boosting-my-user/task.R
+++ wf4-biomass-extreme-gradient-boosting-my-user/task.R
@@ -74,14 +74,14 @@ if (!requireNamespace("xgboost", quietly = TRUE)) {
 	install.packages("xgboost", repos="http://cran.us.r-project.org")
 }
 library(xgboost)
-if (!requireNamespace("tidyr", quietly = TRUE)) {
-	install.packages("tidyr", repos="http://cran.us.r-project.org")
-}
-library(tidyr)
 if (!requireNamespace("scales", quietly = TRUE)) {
 	install.packages("scales", repos="http://cran.us.r-project.org")
 }
 library(scales)
+if (!requireNamespace("tidyr", quietly = TRUE)) {
+	install.packages("tidyr", repos="http://cran.us.r-project.org")
+}
+library(tidyr)
 if (!requireNamespace("tools", quietly = TRUE)) {
 	install.packages("tools", repos="http://cran.us.r-project.org")
 }
@@ -158,9 +158,6 @@ print("Running the cell")
 cat("R version: ", R.version.string, "\n")
 
 cat("R_ENABLE_ALTREP =", Sys.getenv("R_ENABLE_ALTREP"), "\n")
-
-x <- 1:1e7  # vettore lungo
-typeof(x)    # deve essere "integer", non ALTREP pointer
 
 library(ggplot2)
 library(xgboost)
@@ -441,8 +438,245 @@ model_info <- list(
 )
 
 saveRDS(model_info, file = file.path(model_dir, "model_info.rds"))
+
+
+
+
+library(caret)
+library(ggplot2)
+library(iml)
+library(xgboost)
+
+config_base_path <- "/tmp/data/WF4"
+
+
+model_info_path <- file.path(model_dir, "model_info.rds")
+model_info <- readRDS(model_info_path)
+
+model_file <- model_info$model_file
+preProcess <- model_info$preProcess
+train_data <- model_info$train_data
+test_data <- model_info$test_data
+predictors <- model_info$predictors
+target_variable <- model_info$target_variable
+
+cat("Model file: ", model_file, "\n")
+
+ext <- tools::file_ext(model_file)
+
+cat("Ext file: ", ext, "\n")
+
+data_for_predictor <- train_data[, predictors, drop = FALSE]
+test_data_proc <- test_data[, predictors, drop = FALSE]
+
+if (ext == "xgb") {
+    message("Loading XGBoost binary model: ", model_file)
+
+    print(file.info(model_file)$size)
+
+    
+    best_model <- xgboost::xgb.load(model_file)
+
+    if (!is.null(preProcess)) {
+        message("Applying preProcess to training data")
+        data_for_predictor <- predict(preProcess, train_data[, predictors])
+        test_data_proc <- predict(preProcess, test_data[, predictors])
+        predictors <- colnames(data_for_predictor)
+    }
+} else if (ext == "rds") {
+    message("Loading RDS model: ", model_file)
+    best_model <- readRDS(model_file)
+} else {
+    stop("Unsupported model file type: ", ext)
+}
+
+
+
+
+
+datasets <- c("train", "test")
+
+for (ds in datasets) {
+
+    cat("current dataset: ", ds)
+
+    dataset = data_for_predictor
+    if (ds == "test") {
+        dataset = test_data_proc
+    }
+
+    shap_dir <- file.path(model_dir, paste("SHAP", ds, sep = "_"))
+    if (!dir.exists(shap_dir)) {
+      dir.create(shap_dir, recursive = TRUE)
+    }
+
+    message("Columns in data_for_predictor: ", paste(colnames(data_for_predictor), collapse = ", "))
+    message("Columns in dataset row: ", paste(colnames(dataset), collapse = ", "))
+
+    cat(predictors, "\n")
+
+    if (inherits(best_model, "xgb.Booster")) {
+      predict_fun <- function(model, newdata) {
+        xgb_data <- as.matrix(newdata)
+        predict(model, xgb_data)
+      }
+    } else {
+      predict_fun <- function(model, newdata) {
+        predict(model, newdata)
+      }
+    }
+
+    
+    predictor_model <- Predictor$new(best_model, data = data_for_predictor, y = train_data[[target_variable]], predict.fun = predict_fun)
+    
+
+    shap_values_all <- lapply(1:nrow(dataset), function(i) {
+        
+        x_interest <- dataset[i, predictors, drop = FALSE]
+
+        str(x_interest)
+        
+        Shapley$new(predictor_model, x.interest = x_interest)
+    })
+    
+    
+    shap_values_matrix <- sapply(shap_values_all, function(shap_obj) {
+      if (!is.null(shap_obj$results)) {
+        return(as.numeric(shap_obj$results$phi))  # Assuming that 'phi' contains SHAP values
+      } else {
+        return(rep(NA, length(predictors)))  # Returns NA if there are no SHAP values
+      }
+    })
+    
+    shap_values_matrix <- t(shap_values_matrix)
+    
+    shap_values_df <- as.data.frame(shap_values_matrix)
+    
+    colnames(shap_values_df) <- predictors  # 'predictors' è la lista dei nomi delle variabili
+    
+    mean_absolute_shap <- apply(shap_values_df, 2, function(x) mean(abs(x), na.rm = TRUE))
+    
+    sorted_shap <- sort(mean_absolute_shap, decreasing = TRUE)
+    
+    
+    shap_df_sorted <- data.frame(
+      Variable = names(sorted_shap),
+      MeanAbsSHAP = sorted_shap
+    )
+    
+    shap_data_file <- file.path(shap_dir, "shap_data_importance.csv")  
+    write.csv(shap_df_sorted, shap_data_file, row.names = FALSE)
+    
+    bar_plot <- ggplot(shap_df_sorted, aes(x = reorder(Variable, -MeanAbsSHAP), y = MeanAbsSHAP)) +
+      geom_bar(stat = "identity", fill = "steelblue") +
+      labs(
+        title = "Feature Impact on Model Output (SHAP Values) ",
+        x = "Features",
+        y = "Mean SHAP value"
+      ) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    
+    
+    png_file <- file.path(shap_dir, "shap_importance_bar_plot.png")          
+    ggsave(filename = png_file, 
+           plot = bar_plot, width = 8, height = 6)
+    
+    shap_data <- data.frame(
+      SHAP = unlist(lapply(shap_values_all, function(shap_obj) shap_obj$results$phi)),
+      Variable = rep(names(sorted_shap), each = nrow(shap_values_df)),
+      FeatureValue = unlist(lapply(shap_values_all, function(shap_obj) shap_obj$results$feature.value)),
+      FeatureNumericValue = unlist(lapply(shap_values_all, function(shap_obj) {
+        as.numeric(sub(".*=", "", shap_obj$results$feature.value))  # Extracts only the numeric value of the variable
+      }))
+    )
+    
+    shap_data_file <- file.path(shap_dir, "shap_data_summary_plot.csv")  
+    write.csv(shap_data, shap_data_file, row.names = FALSE)
+    
+    shap_data$Variable <- factor(shap_data$Variable, levels = names(sorted_shap))
+    
+    shap_summary_plot <- ggplot(shap_data, aes(x = SHAP, y = Variable)) +
+      geom_point(aes(color = FeatureNumericValue), alpha = 0.7) +  # Use the variable value for the color
+      scale_color_gradientn(
+        colors = c("red", "yellow", "green"),  # Colors: red for low intensity, yellow for neutral, green for high intensity
+        values = scales::rescale(c(min(shap_data$FeatureNumericValue), median(shap_data$FeatureNumericValue), max(shap_data$FeatureNumericValue))),  # Rescale based on the range for each variable
+        limits = c(min(shap_data$FeatureNumericValue), max(shap_data$FeatureNumericValue)),  # Bounds between the minimum and maximum SHAP value for each variable
+        breaks = c(min(shap_data$FeatureNumericValue), median(shap_data$FeatureNumericValue), max(shap_data$FeatureNumericValue)),  # Color scale breaking points
+        labels = c("Low", "Neutral", "High")  # Labels for the extreme values of the coloring
+      ) +
+      labs(
+        title = "SHAP Summary Plot of Feature Importance",
+        x = "SHAP value",
+        y = "Feature",
+        color = "Feature value"
+      ) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      theme(legend.position = "right")
+    
+    png_file <- file.path(shap_dir, "shap_summary_plot.png")
+    ggsave(filename = png_file, 
+           plot = shap_summary_plot, width = 8, height = 6)
+}
+
+output_dir <- config_base_path
+
+
+
+
+
+library(caret)
+library(ggplot2)
+library(tidyr)
+
+config_base_path <- "/tmp/data/WF4"
+
+
+model_info_path <- file.path(model_dir, "model_info.rds")
+model_info <- readRDS(model_info_path)
+
+model_file <- model_info$model_file
+test_data <- model_info$test_data
+target_variable <- model_info$target_variable
+uom <- model_info$target_variable_uom
+
+cat("Model file: ", model_file, "\n")
+
+best_model  <- readRDS(model_file)
+
+
+predictions_test <- predict(best_model, newdata = test_data)
+
+results_test_df <- data.frame(
+  Observed = test_data[[target_variable]],
+  Predicted = predictions_test
+)
+
+results_test_long <- results_test_df %>%
+  pivot_longer(cols = c("Observed", "Predicted"),
+               names_to = "Type",
+               values_to = "Value")
+
+violin_plot <- ggplot(results_test_long, aes(x = Type, y = Value, fill = Type)) +
+  geom_violin(trim = FALSE) +  # Show all distribution
+  scale_fill_manual(values = c("blue", "red")) +  # Set colors for actual and predicted data
+  labs(title = paste("Observed vs Predicted Values ", target_variable),
+       x = " Data Type",
+       y = paste("Value of ", target_variable, "(", uom, ")")) +
+  theme_minimal() +
+  theme(legend.position = "none")  # Remove the legend
+
+print(violin_plot)
+
+plot_path_violin_test <- file.path(model_dir, "violin_plot_test_set.png")
+ggsave(filename = plot_path_violin_test, plot = violin_plot, width = 8, height = 6)
+
+cat("Violin plot saved in:", plot_path_violin_test, "\n")
+
+output_dir <- config_base_path
 # capturing outputs
-print('Serialization of model_dir')
-file <- file(paste0('/tmp/model_dir_', id, '.json'))
-writeLines(toJSON(model_dir, auto_unbox=TRUE), file)
+print('Serialization of output_dir')
+file <- file(paste0('/tmp/output_dir_', id, '.json'))
+writeLines(toJSON(output_dir, auto_unbox=TRUE), file)
 close(file)
